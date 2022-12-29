d9bc8602acb2b77a6c5de4e25493370c5dcc885d
strahil9810 <strahil1998@abv.bg>
2022-12-29 15:31:50 +0200

    More task

--- perfect-diamand.js
+++ perfect-diamand.js
@@ -1,16 +1,16 @@
-function perfectDiamand(input){
-    let n=Number(input);
+function perfectDiamand(input) {
+    let n = Number(input);
 
-    for(let i=0;i<n;i++){
-        console.log(' '.repeat(n-i-1) + 
-        '*' + '-*'.repeat(i) + 
-        ' '.repeat(n-i-1));
+    for (let i = 0; i < n; i++) {
+        console.log(' '.repeat(n - i - 1) +
+            '*' + '-*'.repeat(i) +
+            ' '.repeat(n - i - 1));
     }
 
-    for(let i=n-2;i>=0;i--){
-        console.log(' '.repeat(n-i-1) +
-        '*' + '-*'.repeat(i) +
-        ' '.repeat(n-i-1));
+    for (let i = n - 2; i >= 0; i--) {
+        console.log(' '.repeat(n - i - 1) +
+            '*' + '-*'.repeat(i) +
+            ' '.repeat(n - i - 1));
     }
 }
 perfectDiamand(5)

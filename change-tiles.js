946300f42142080133162fd7ffa1f48ea723dc84
strahil9810 <strahil1998@abv.bg>
2023-01-01 12:35:07 +0200

    More task

---
+++ change-tiles.js
@@ -0,0 +1,25 @@
+function tiles(input) {
+    let sum = Number(input[0]);
+    let underWidth = Number(input[1]);
+    let underLong = Number(input[2]);
+    let tileSide = Number(input[3]);
+    let tileHieght = Number(input[4]);
+    let priceOneTile = Number(input[5]);
+    let sumMaster = Number(input[6]);
+
+    let underArea = underWidth * underLong;
+    let tileArea = tileSide * tileHieght / 2;
+
+    let needTiles = Math.ceil(underArea / tileArea) + 5;
+    let needSum = needTiles * priceOneTile + sumMaster;
+
+    if (needSum <= sum) {
+        let leftPrice = sum - needSum;
+        console.log(`${leftPrice.toFixed(2)} lv left.`);
+    }
+    else {
+        let needPrice = needSum - sum;
+        console.log(`You'll need ${needPrice.toFixed(2)} lv more.`);
+    }
+}
+tiles([500, 3, 2.5, 0.5, 0.7, 7.80, 100])
